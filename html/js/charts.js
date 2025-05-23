72d998438d468391ac9f4476a4421e964f2cecd9
ZorzeteSPtech <matheus.pereira@sptech.school>
2025-05-23 08:32:30 -0300

    Atualização da parte de ranking

---
+++ html/js/charts.js
@@ -0,0 +1,42 @@
+
+const ctxPartida = document.getElementById('partidas');
+const ctxJogadas = document.getElementById('partidasJogadas');
+
+new Chart(ctxPartida, {
+    type: 'bar',
+    data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
+
+
+new Chart(ctxJogadas, {
+    type: 'line',
+    data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
